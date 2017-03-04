a67c8ef00275a01b5412ca38230ec29cd99e4468
Shreyas Gopal <shreyas.gopal.ind@gmail.com>
2017-03-04 10:58:01 +0530

    Updated beautifier
    Additional comments added.

--- beautifier.py
+++ beautifier.py
@@ -1,9 +1,17 @@
+#The only function in this module is the printer function.
+#Functions as a pretty printer for Nested Dictionaries and Nested Lists
+
+#Recursive Function printer. Accpts the structure and the number of tabs so far.
 def printer(a, tab = 0):
+	#Handling lists
 	if type(a) == type(list()):
 		for i in a:
 			printer(i, tab)
+	#Handling dictionaries
 	elif type(a) == type(dict()):
+		#Scans all the elements in the dictionary
 		for i in a:
+			#Recursively calls the printer function with the new data structure and an incremented tab count.
 			if(type(a[i]) == type(dict())):
 				for j in xrange(tab):
 					print '\t',
@@ -18,6 +26,7 @@ def printer(a, tab = 0):
 				for j in xrange(tab):
 					print '\t',
 				print i, ': ', a[i]
+	#Handling any other type format
 	else:
 		for i in xrange(tab+1):
 			print '\t',
